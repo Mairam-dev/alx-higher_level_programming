1323adc4bd7e6ac29c78fb1651cbf2bf1d304e29
Mairam-dev <diey333@gmail.com>
2022-06-07 21:36:37 +0000

    update 13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,51 +1,48 @@
 #include "lists.h"
-#include <stdlib.h>
-#include <stdio.h>
-/*
- * is_palindrome - function with one argument
- * @head: pointer to linked list
- *
- * Description: check if value singly linked list is palindrome
- * Return: 1 if true or 0 if false
+
+/**
+ * is_palindrome - tests if linked lists is palindrome
+ * @head: address of pointer to list
+ * Return: 1 is palindrome else 0
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *ptr;
-	int count = 0, count2 = 0, half;
-	int *buf = NULL;
+	listint_t *slow = *head, *fast = *head, *node, *prev;
+	int failed = 0;
 
-	if (!head)
-	      return (0);
-	if (!*head)
-		return (1);
-	ptr = *head;
-	while (ptr && ptr->next)
+	while (fast != NULL && fast->next != NULL)
 	{
-		ptr = ptr->next;
-		count++;
+		fast = fast->next->next;
+		slow = slow->next;
 	}
-	buf = malloc(sizeof(int) * count);
-	if (!buf)
-		return (0);
-
-	ptr = *head;
-	count = 0;
-	while (ptr)
+	node = slow;
+	prev = NULL;
+	while (node)
 	{
-		buf[count] = ptr->n;
-		count++;
-		ptr = ptr->next;
+		fast = node->next;
+		node->next = prev;
+		prev = node;
+		node = fast;
 	}
-	half = count / 2;
-
-	while (half)
+	fast = *head;
+	node = prev;
+	while (prev)
 	{
-		if (buf[count2] != buf[count - 1])
-			return (0);
-		half--;
-		count2++;
-		count--;
+		if (fast->n != prev->n)
+		{
+			failed = 1;
+			break;
+		}
+		fast = fast->next;
+		prev = prev->next;
 	}
-	free(buf);
-	return (1);
+	prev = NULL;
+	while (node)
+	{
+		fast = node->next;
+		node->next = prev;
+		prev = node;
+		node = fast;
+	}
+	return (!failed);
 }
