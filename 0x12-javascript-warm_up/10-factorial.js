03b4e8a23a0f4d04f3eaee895e12f80085303af9
Mairam-dev <diey333@gmail.com>
2023-01-26 15:19:35 +0000

    change file 10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,16 @@
 #!/usr/bin/node
-// computes and prints a factorial
-
-function factorial (n) {
-  if ((isNaN(n)) || (n === 1)) {
+function factorial (number) {
+  if (isNaN(number)) {
+    return 1;
+  }
+  if (number < 0) {
+    return -1;
+  }
+  if (number === 0) {
     return 1;
   } else {
-    return n * factorial(n - 1);
+    return number * factorial(number - 1);
   }
 }
 
-console.log(factorial(parseInt(process.argv[2])));
+console.log(factorial(process.argv[2]));
