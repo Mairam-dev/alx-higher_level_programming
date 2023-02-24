c0bb6f1d71c5583b824a6d244d6e1649945f2429
Mairam-dev <diey333@gmail.com>
2023-02-24 17:55:18 +0000

    change file 4-rectangle.js

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,27 +1,30 @@
 #!/usr/bin/node
-// JS script to define a class, creates an instance method to print rectangle
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
-      return this;
-    } else {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
+
   print () {
-    for (let count = 0; count < this.height; count++) {
-      console.log('X'.repeat(this.width));
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write('X');
+      }
+      console.log();
     }
   }
+
   rotate () {
-    let holder = this.width;
+    const tmp = this.width;
     this.width = this.height;
-    this.height = holder;
+    this.height = tmp;
   }
+
   double () {
-    this.width = 2 * this.width;
-    this.height = 2 * this.height;
+    this.width = this.width * 2;
+    this.height = this.height * 2;
   }
 }
 module.exports = Rectangle;
