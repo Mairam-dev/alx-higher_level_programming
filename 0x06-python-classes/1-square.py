01c52f6408b8f7bc19a82f6a4bb728fb06ac90b1
Mairam-dev <diey333@gmail.com>
2023-03-22 18:01:59 +0000

    change file 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,16 +1,8 @@
 #!/usr/bin/python3
+
+
 class Square:
-    """Square class for a text square.
-    Attributes:
-       __size: size of the square privately
-    """
-    __size = 0
+    """This is class defines a square """
 
     def __init__(self, size):
-        """__init__ method to initialize class
-        Args:
-        self: the object itself
-        size (int): the size of the square
-        Returns: No Value
-        """
         self.__size = size
